7e03058a68bb732c09c44e274a616f6e60b5847e
Aslam Najeebdeen <aslamnajeem@gmail.com>
2011-11-18 18:01:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CompScraper
+===========
+Simple scraper script I developed to help one of my friend's tedious copy
+paste job
---
+++ setup.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+output_file = File.new('output.txt', 'w')
+
+if FileTest.exists?('input.txt')
+  File.open('input.txt', 'r') do |input_file|
+
+    while line = input_file.gets
+      line.chomp.each_line do |url|
+        begin
+          puts "Fetching: #{url}"
+          doc = Nokogiri::HTML(open(url))
+          company_url = doc.at_css("small.company_site").text
+          puts "Fetched: #{ company_url }"
+          output_file.puts company_url
+        rescue
+          puts "***** Invalid URL *****"
+          output_file.puts "***** A Company URL failed to fetched. Recheck your Inc profile URL *****"
+          exit
+        end
+      end
+    end
+
+  end
+else
+  puts "================================================================="
+  puts "'input.txt' file is missing"
+  File.open('input.txt', 'w').close
+  puts "We created the file for you. :)"
+  puts "Paste the Inc company URLs to the file, and run the setup again."
+  puts "================================================================="
+end
+
+
+output_file.close
